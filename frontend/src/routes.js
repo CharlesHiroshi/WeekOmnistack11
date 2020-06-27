6d4fa0a51420b8ef86bf72ba9066245dfb725768
Charles Hiroshi <charles_hiroshi@outlook.com>
2020-06-27 22:54:26 +0900

    Defined route for Logon.js (comments)

--- frontend/src/routes.js
+++ frontend/src/routes.js
@@ -1,9 +1,10 @@
-import React from 'react';
-import { BrowserRouter, Route, Switch } from "react-router-dom";
-import Logon from './pages/Logon';
-import Register from './pages/Register';
-import Profile from './pages/Profile';
-import NewIncident from "./pages/NewIncident";
+import React from 'react'
+/* ↓ Importação do React DOM para as rotas */
+import { BrowserRouter, Route, Switch } from "react-router-dom"
+import Logon from './pages/Logon'
+import Register from './pages/Register'
+import Profile from './pages/Profile'
+import NewIncident from "./pages/NewIncident"
 
 export default function Routes() {
   return (
